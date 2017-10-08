e053ebca094bcc75ca5bee347cfbb6a608b08168
bob1981 <zzt1981@126.com>
2017-10-08 09:03:21 +0800

    update

--- spring-cloud-eureka-server/src/main/java/com/example/demo/Application.java
+++ spring-cloud-eureka-server/src/main/java/com/example/demo/Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudEurekaServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
