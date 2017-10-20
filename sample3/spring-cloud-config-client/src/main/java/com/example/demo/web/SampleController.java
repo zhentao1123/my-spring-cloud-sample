833a80ff1f2b7b2d1931af9b5dba9da377cf524a
Bob Zhang <zzt1981@126.com>
2017-10-20 14:42:59 +0800

    Update SampleController.java

--- sample3/spring-cloud-config-client/src/main/java/com/example/demo/web/SampleController.java
+++ sample3/spring-cloud-config-client/src/main/java/com/example/demo/web/SampleController.java
@@ -3,19 +3,30 @@ package com.example.demo.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class SampleController {
 	
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@Value("${from}")
-	String profile;
+	String from;
 	
-	@RequestMapping(value="readConfig")
-	public String readConfig() {
-		return profile;
+	@RequestMapping(value="from")
+	public String from() {
+		return from;
 	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	
 }
