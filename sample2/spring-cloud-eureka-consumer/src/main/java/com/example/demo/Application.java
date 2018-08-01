ad4b98f73712608afe3f59d312253b96da03864c
bob1981 <zzt1981@126.com>
2018-08-02 07:33:56 +0800

    create

--- sample2/spring-cloud-eureka-consumer/src/main/java/com/example/demo/Application.java
+++ sample2/spring-cloud-eureka-consumer/src/main/java/com/example/demo/Application.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
